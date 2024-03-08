40215232077db140dbf3d3f1d0e5139f8033395c
Peon3 <gerndtle@gmail.com>
2024-03-08 09:46:58 +0100

    kekw

---
+++ dave.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+from queue_states import all_queue_state
+
+# what does dave need to do?
+# read an inputfile in cwd, create an instance (logfile) of project for daemon in daemons_basement
+# parse the roadmap and somehow bundle informations in that sense, that daemon can read and execute tasks later on
+
+# Next take the geoms and build directory 
+# Write the initial input files, sub calculations (likely just utilizing suborca.py or 5suborca1 on liverpool)
+# 
+
+
+print(all_queue_state())
