b2f04fab3927c523875c4472e40b127bfa998d19
gerndleo <gerndleo@hu-berlin.de>
2024-04-24 21:48:03 +0200

    made calc_types more modular, though left open problems

--- calc_types.py
+++ calc_types.py
@@ -2,28 +2,45 @@
 import os
 from os.path import isfile, isdir
 
+linear_modes = ['opt', 'freq', 'optts'] 
+branch_modes = ['neb', 'irc', 'cc']
+
 def main():
-	freq_from_geom(nprocs='33', maxcore='12000', add='\n%geom\ninhess read\ninhessname "in.hess"\nend\n')
+    setup_followup(nprocs='33', maxcore='12000', add='\n%geom\ninhess read\ninhessname "in.hess"\nend\n')
 	#print("this is a lib")
 
-def freq_from_geom(**kwargs):
+def setup_dir(new_calc_type, mode, *args):
     try:
-        os.mkdir("./freq/")
-        yesno = 'n'
+        if mode == 'linear':
+            os.mkdir('./'+new_calc_type+'/')
+        elif mode == 'branch':
+            for dir_name in args:
+                os.mkdir('./'+dir_name+'/')
     except FileExistsError:
-        print("Frequency calculation directory exists, do you want to run another freq calc? [y/n]")
-        yesno = input("")
-        if yesno == 'y':
-            print("Calc will be conducted in freq_1")
-            os.mkdir("./freq_1")
-        if yesno == 'n':
-            print("We will stop here.")
-            quit()
+        return FileExistsError
+
+def write_inp(prev_calc_type, new_calc_type, prev_inp_file, **kwargs):
+    try:
+        with open(prev_inp_file, 'r') as file:
+            prev_inp_str = file.readlines()
+        for line in prev_inp_str:
+            for type in pos_calc_types:
+                if type.casefold() in line.casefold():
+                    prev_inp_str[prev_inp_str.index(line)] = '! TightSCF '+ new_calc_type + '\n'
+    except Exception:
+        return Exception
+    
+def setup_followup(prev_calc_type, new_calc_type, prev_inp_file, **kwargs):
+    try:
+        if new_calc_type in linear_modes:
+            mode = 'linear'
+        elif new_calc_type in branch_modes:
+            mode = 'branch'
+        setup_dir(new_calc_type, mode)
+        write_inp(prev_calc_type, new_calc_type, prev_inp_file, kwargs)
+    except Exception:
+        return Exception
     cwd = os.getcwd()
-    if yesno == 'y':
-         newdir = 'freq_1/'
-    else:
-         newdir = 'freq/'
     prev_inp_file = [f for f in os.listdir(cwd) if isfile(f) and '.inp' in f][0]
     with open(prev_inp_file, 'r') as file:
          prev_inp_str = file.readlines()
@@ -33,7 +50,7 @@ def freq_from_geom(**kwargs):
          for key in kwargs.keys():
               if key.casefold() in line.casefold():
                    prev_inp_str[prev_inp_str.index(line)] = key +' '+ str(kwargs[key]) +'\n'
-    with open(newdir+"experimental.inp", 'w') as file:
+    with open(newdir+prev_inp_file, 'w') as file:
          for line in prev_inp_str:
               if '!' in line:
                    file.write(line)
