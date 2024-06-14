a65b7ea1c9f92ccec194dfcb4380a2396be549de
Peon <gerndleo@hu-berlin.de>
2024-06-14 22:22:02 +0200

    things are happening

--- simple.py
+++ simple.py
@@ -1,9 +1,47 @@
 #! usr/bin/python3
 import argparse
 import os, sys
+from lib_keyword_dict import create_keyword_dict
+
+class input_info_objects:
+    def __init__(self, **kwargs):
+        if len(kwargs) != self.required_attributes:
+            raise TypeError(f"Excpected {self.required_attributes} arguments, recieved {len(kwargs)}")
+        
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        
+        required_attributes = 3
 
 
-def read_inp() -> list:
+def read_write_inp() -> list:
+    parser = argparse.ArgumentParser(add_help=False, formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('file', nargs='1',
+                        help='''standard orca inp''')
+    parser.add_argument('new_basis', nargs='1',
+                        help='''some basis in \'basis\' please ''')
+    parser.add_argument('-h', '--help', action='help', default=argparse.SUPPRESS, help='Show this help message and exit.')
+    args = parser.parse_args()
+
+    if len(args.file) != 1:
+        print("So far the program can handle just one file at a time")
+        sys.exit()
+    else:
+        new_lines = []
+        keyword_dict = create_keyword_dict
+        with open(args.file[0], 'r') as set_file:
+            all_lines = set_file.readlines()
+        for line in all_lines:
+            for entry in line.split():
+                if entry.casefold() in keyword_dict and keyword_dict[entry.casefold()] == 'basis'.casefold() and '!' in line.casefold():
+                    new_lines.add('! '+args.new_basis) 
+                elif entry.casefold() in keyword_dict and '!' in line.casefold():
+
+
+
+    return info_obj
+
+""" def read_inp_help() -> list:    # tis for later to adapt, for now we go for a simple approach
     parser = argparse.ArgumentParser(add_help=False, formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('files', nargs='*',
                         help='''Input file format can be found in you mom''')
@@ -14,15 +52,17 @@ def read_inp() -> list:
         print("So far the program can handle just one file at a time")
         sys.exit()
     else:
-        info_obj = []
+        info_obj = dict()
+        keyword_dict = create_keyword_dict
         with open(args.files[0], 'r') as set_file:
-            info_obj = set_file.readlines()
-        for line in info_obj:
+            all_lines = set_file.readlines()
+        for line in all_lines:
             for entry in line.split():
-                if entry.casefold() in
-    return info_obj
+                if entry.casefold() in keyword_dict and keyword_dict[entry.casefold()] == 'basis'.casefold():
+                    info_obj['basis'] = 
+    return info_obj """
 
-def read_out() -> list:
+""" def read_out() -> list:
     parser = argparse.ArgumentParser(add_help=False, formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('files', nargs='*',
                         help='''Input file format can be found in you mom''')
@@ -36,6 +76,6 @@ def read_out() -> list:
         info_obj = []
         with open(args.files[0], 'r') as set_file:
             
-    return info_obj
+    return info_obj """
 
 def set_up_geom():
